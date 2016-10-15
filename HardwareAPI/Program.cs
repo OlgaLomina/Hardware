7eaeee53834117f5cea0ff0e99e662501303c8a9
OlgaLomina <olgalomina@outlook.com>
2016-10-14 21:09:52 -0700

    Initial commit

---
+++ HardwareAPI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareAPI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //var device = new InputDevice();
+            //device.DeviceName = "KeyBoard";
+            //device.Brand = "Logitech";
+            //device.Model = "MB110LL/B";
+            //device.SerialNumber = "1508LZ00AR68";
+            //device.Count = 10;
+            //Stock.AddInputDevice(device);
+
+            //device = new InputDevice();
+            //device.DeviceName = "KeyBoard";
+            //device.Brand = "Insignia";
+            //device.Model = "MK200";
+            //device.SerialNumber = "6108LZ0034DF";
+            //device.Count = 35;
+            //Stock.AddInputDevice(device);
+
+            //Stock.CreateInputDevices();
+
+
+        }
+
+    }
+}
+
---
+++ HardwareAPI/Stock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareAPI
+{
+    static class Stock
+    {
+        #region Properties
+
+        public static string Name { get; set; }
+
+        public static string Address { get; set; }
+
+        // Collection of Input Devices
+        public static List<InputDevice> InputDevices { get; set; }
+
+        #endregion
+
+        #region Constructors
+        static Stock()
+        {
+            InputDevices = new List<InputDevice>();
+        }
+        #endregion
+
+        #region Methods
+        public static void AddInputDevice(InputDevice device)
+        {
+            InputDevices.Add(device);
+
+        }
+                
+        public static void CreateInputDevices()
+        {
+            foreach (var device in InputDevices)
+            {
+                Console.WriteLine("DeviceName: {0}, Brand: {1}, Model: {2}, SerialNumber: {3}",
+                    device.DeviceName, device.Brand, device.Model, device.SerialNumber);
+                //Console.Read();
+
+                
+            }
+
+        }
+        #endregion
+
+
+
+    }
+}
